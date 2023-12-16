29d83f764a6030fe6482ad9d29d79fca8bf8582d
bogdan.marcu <bogdanmarcu2002@icloud.com>
2023-12-16 12:44:22 +0200

    [issue-8] try to fetch data

--- my-remix-app/app/routes/appointment-list.tsx
+++ my-remix-app/app/routes/appointment-list.tsx
@@ -1,29 +1,33 @@
 import type {LoaderFunction} from "@remix-run/node";
 import {requireUserId} from "~/utils/auth.server";
+import {prisma} from "~/utils/prisma.server";
+
+const appointment = await prisma.appointment.findMany()
+console.log(appointment)
 
 export const loader: LoaderFunction = async ({request}) => {
     await requireUserId(request)
-
     return null
 }
-var arrayForDisplayPurpose = [
-    {
-        "date": "1-3-2022",
-        "time": "19:15git",
-        "client": "Client1",
-        "workoutType": "MUSCLE"
-    },{
-        "date": "4-2-2023",
-        "time": "10:30",
-        "client": "Client2",
-        "workoutType": "ENDURANCE"
-    },{
-        "date": "1-2-2021",
-        "time": "12:25",
-        "client": "Client3",
-        "workoutType": "CARDIO"
-    }
-]
+// var arrayForDisplayPurpose = [
+//     {
+//         "date": "1-3-2022",
+//         "time": "19:15git",
+//         "client": "Client1",
+//         "workoutType": "MUSCLE"
+//     },{
+//         "date": "4-2-2023",
+//         "time": "10:30",
+//         "client": "Client2",
+//         "workoutType": "ENDURANCE"
+//     },{
+//         "date": "1-2-2021",
+//         "time": "12:25",
+//         "client": "Client3",
+//         "workoutType": "CARDIO"
+//     }
+// ]
+
 export default function AppointmentList(){
     return (
         <div>
@@ -38,16 +42,16 @@ export default function AppointmentList(){
                     </tr>
                 </thead>
                 <tbody>
-                {arrayForDisplayPurpose.map((item)=>{
-                    return(
-                        <tr>
-                            <td>{item.date}</td>
-                            <td>{item.time}</td>
-                            <td>{item.client}</td>
-                            <td>{item.workoutType}</td>
-                        </tr>
-                    )
-                })}
+                {/*{appointment.map((item)=>{*/}
+                {/*    return(*/}
+                {/*        <tr>*/}
+                {/*            <td>{item.date}</td>*/}
+                {/*            <td>{item.time}</td>*/}
+                {/*            <td>{item.client}</td>*/}
+                {/*            <td>{item.workoutType}</td>*/}
+                {/*        </tr>*/}
+                {/*    )*/}
+                {/*})}*/}
                 </tbody>
             </table>
         </div>
