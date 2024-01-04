b378361234314159551ce7c74eb79ad75608fc6c
Reynk <robertadrian.chelaru@gmail.com>
2024-01-04 15:26:58 +0200

    add "add appointment" component and form in the client ID route
    (functionality not implemented)

---
+++ my-remix-app/app/components/add-appointment.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { FormField } from "~/components/form-field";
+import { Select } from "~/components/select-field";
+import { useActionData, useFetcher } from "@remix-run/react";
+import { useTranslation } from "~/utils/useTranslation";
+
+export default function AddAppointment() {
+    const { t } = useTranslation();
+    const actionData = useActionData() as any;
+    const fetcher = useFetcher();
+    const [formData, setFormData] = useState({
+        date: '',
+        time: '',
+        workoutType: '',
+    });
+
+    useEffect(() => {
+        if (actionData == 'Success adding appointment') {
+            setFormData({
+                date: '',
+                time: '',
+                workoutType: '',
+            });
+        }
+    }, [actionData]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>, field: string) => {
+        setFormData(form => ({ ...form, [field]: event.target.value }));
+    };
+
+    return (
+        <fetcher.Form method="post" action='/add-appointment' className="bg-gray-200 p-6">
+            <div className='flex justify-between items-center'>
+                <div>
+                    <FormField
+                        label={t('date')}
+                        type="date"
+                        value={formData.date}
+                        onChange={(e) => handleInputChange(e, 'date')}
+                        htmlFor={""} />
+                    <FormField
+                        label={t('hour')}
+                        type="time"
+                        value={formData.time}
+                        onChange={(e) => handleInputChange(e, 'time')}
+                        htmlFor={""} />
+                    <Select
+                        label={t('workoutType')}
+                        value={formData.workoutType}
+                        onChange={(e) => handleInputChange(e, 'workoutType')}
+                        options={["Cardio", "Endurance", "Muscle"]}
+                        htmlFor={""} />
+                    <button type="submit" name="_action"
+                        className="rounded-xl bg-yellow-300 p-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+                        {t('add')}
+                    </button>
+                </div>
+            </div>
+        </fetcher.Form>
+    );
+}
